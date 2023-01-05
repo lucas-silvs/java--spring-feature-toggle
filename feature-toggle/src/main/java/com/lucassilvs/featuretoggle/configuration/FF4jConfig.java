f4823d1066cc93ec2c9ad09888d88f202c343548
Lucas Santos <lucas.parkl@outlook.com>
2023-01-05 00:00:48 -0300

    feat: utilizando MongoDB: - adicionando integração com DynamoDB

--- feature-toggle/src/main/java/com/lucassilvs/featuretoggle/configuration/FF4jConfig.java
+++ feature-toggle/src/main/java/com/lucassilvs/featuretoggle/configuration/FF4jConfig.java
@@ -1,34 +1,41 @@
 package com.lucassilvs.featuretoggle.configuration;
 
 import org.ff4j.FF4j;
-import org.ff4j.audit.repository.InMemoryEventRepository;
-import org.ff4j.property.store.InMemoryPropertyStore;
+import org.ff4j.core.FeatureStore;
+import org.ff4j.dynamodb.feature.FeatureStoreDynamoDB;
+import org.ff4j.dynamodb.property.PropertyStoreDynamoDB;
+import org.ff4j.property.store.PropertyStore;
 import org.ff4j.security.SpringSecurityAuthorisationManager;
-import org.ff4j.store.InMemoryFeatureStore;
-import org.ff4j.web.controller.HomeController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 /***
  * Bean responsavel pela configuração do FF4J, como conexão com banco, autoCreate, cache, etc.
  */
 @Configuration
 public class FF4jConfig {
-    /** Some logger. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-
+    /**
+     * Some logger.
+     */
+    // private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
     @Bean
-    public FF4j getFF4j() {
+    public FF4j getFF4j(DynamoDbClient dynamoDbClient) {
         FF4j ff4j = new FF4j();
+
+
+        FeatureStore featureStore = new FeatureStoreDynamoDB(dynamoDbClient);
+        PropertyStore propertyStore = new PropertyStoreDynamoDB(dynamoDbClient);
+
+
+
 
         /*
          * criando um armazenamento de features em memória
          */
-        ff4j.setFeatureStore(new InMemoryFeatureStore());
-        ff4j.setPropertiesStore(new InMemoryPropertyStore());
-        ff4j.setEventRepository(new InMemoryEventRepository());
+        ff4j.setFeatureStore(featureStore);
+        ff4j.setPropertiesStore(propertyStore);
+
 
         // ativando auditoria e monitoramento, valor padrão é false
         ff4j.audit(true);
